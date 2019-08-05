e6700be92d1ea937660ebdae0798e6048202f1bd
jacobsilver2 <jacobsilver2@mac.com>
2019-08-05 18:25:47 -0400

    got some basic auth going on

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,64 +1,61 @@
-import React from 'react';
-import { StaticQuery, graphql, } from 'gatsby';
-import Layout from '../components/layout';
-import GridItem from '../components/GridItem';
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import GridItem from "../components/GridItem"
 
-const WorkAllPostQuery = graphql`
-query WorkAllPostQuery {
-      allWordpressPost(filter: { categories: { elemMatch: {name: {eq: "project"}}}} sort: { fields: [date], order:ASC }) {
-        edges {
-          node {
-            date(formatString: "DD, MMM YYYY")
-            title
-            id
-            slug
-            featured_media {
-              localFile {
-                publicURL
-                childImageSharp {
-                  id
-                  fluid (maxWidth: 700) {
-                    srcSet
-                    src
-                    aspectRatio
-                    sizes
-                  }
+export const WorkAllPostQuery = graphql`
+  query WorkAllPostQuery {
+    allWordpressPost(
+      filter: { categories: { elemMatch: { name: { eq: "project" } } } }
+      sort: { fields: [date], order: ASC }
+    ) {
+      edges {
+        node {
+          date(formatString: "DD, MMM YYYY")
+          title
+          id
+          slug
+          featured_media {
+            localFile {
+              publicURL
+              childImageSharp {
+                id
+                fluid(maxWidth: 700) {
+                  srcSet
+                  src
+                  aspectRatio
+                  sizes
                 }
               }
-            }            
+            }
           }
         }
       }
     }
-`;
+  }
+`
 
-const Work = () => (
-      <StaticQuery
-        query={WorkAllPostQuery}
-        render={data => (
-          <div>
-            <Layout>
-              {data.allWordpressPost.edges.map(({ node }) => {
-                let imageData = null;
-                if (node.featured_media.localFile.childImageSharp !== null ) {
-                  imageData = node.featured_media.localFile.childImageSharp.fluid
-                }
-                return (
-                <GridItem 
-                  key={node.id} 
-                  slug={node.slug} 
-                  imgData={imageData} 
-                  publicURL={node.featured_media.localFile.publicURL}
-                  title={node.title} 
-                  category="work"
-                />
-              )})}
-            </Layout>
-          </div>
-      )}/>
-);
+const Work = ({ data }) => (
+    <>
+      <Layout>
+        {data.allWordpressPost.edges.map(({ node }) => {
+          let imageData = null
+          if (node.featured_media.localFile.childImageSharp !== null) {
+            imageData = node.featured_media.localFile.childImageSharp.fluid
+          }
+          return (
+            <GridItem
+              key={node.id}
+              slug={node.slug}
+              imgData={imageData}
+              publicURL={node.featured_media.localFile.publicURL}
+              title={node.title}
+              category="work"
+            />
+          )
+        })}
+      </Layout>
+    </>
+)
 
-export default Work;
-
-
-    
+export default Work
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,59 +1,100 @@
-import React from 'react';
-import PropType from 'prop-types';
-import Helmet from 'react-helmet';
-import { graphql } from 'gatsby';
-import Layout from '../components/projectLayout';
+import React, { useState } from "react"
+import PropType from "prop-types"
+import Helmet from "react-helmet"
+import { graphql } from "gatsby"
+import Layout from "../components/projectLayout"
+import Modal from "react-modal"
 
-// const stripHtml = (html) => {
-//   if (typeof window !== 'undefined') {
-//     const doc = new DOMParser().parseFromString(html, 'text/html');
-//     return doc.body.textContent || '';
-//   }
-//   return html;
-// };
+const passWordProtectedSlugs = [
+  "moody-gardens-aquarium",
+  "melab-gallery-in-the-phillip-and-patricia-frost-museum-of-science",
+  "canadian-museum-for-human-rights",
+]
 
+const PostTemplate = props => {
+  console.log(props)
+  const {
+    data: { wordpressPost: post },
+  } = props
+  const [modalIsOpen, setModalIsOpen] = useState(
+    passWordProtectedSlugs.includes(props.data.wordpressPost.slug)
+  )
+  const [password, setPassword] = useState("")
 
-const PostTemplate = (props) => {
-  const { data: { wordpressPost: post } } = props;
+  // function onOpenModal() {
+  //   setModalIsOpen(true)
+  // }
+  function onCloseModal() {
+    setModalIsOpen(false)
+  }
+  // function handleSubmit(e) {
+  //   if (password === 'mary') {
+  //     onCloseModal()
+  //   }
+  // }
+
   return (
     <Layout>
-      <Helmet title={post.title} meta={[ { name: 'description', content: post.excerpt } ]} />
-        <section dangerouslySetInnerHTML={{ __html: post.content }} />
-        {/* <p>{post.content}</p> */}
-    </Layout>            
-  );
-};
+      <Helmet
+        title={post.title}
+        meta={[{ name: "description", content: post.excerpt }]}
+      />
+      <section dangerouslySetInnerHTML={{ __html: post.content }} />
+      {password !== "mary" && (
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={onCloseModal}
+          shouldCloseOnOverlayClick={false}
+          shouldCloseOnEsc={false}
+        >
+          <p>
+            This page is password protected. Please enter the password below to
+            veiw the content.
+          </p>
+          <form>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+            />
+          </form>
+        </Modal>
+      )}
+    </Layout>
+  )
+}
 
 PostTemplate.propTypes = {
   data: PropType.shape({}).isRequired,
-};
+}
 
-export default PostTemplate;
+export default PostTemplate
 
 export const pageQuery = graphql`
   query($id: String!) {
     wordpressPost(id: { eq: $id }) {
-        title
-        content
-        excerpt
-        date(formatString: "DD, MMM YYYY")
-        categories {
+      title
+      content
+      excerpt
+      date(formatString: "DD, MMM YYYY")
+      categories {
+        id
+        name
+      }
+      slug
+      featured_media {
+        localFile {
+          publicURL
+          childImageSharp {
             id
-            name
-        }
-        slug
-        featured_media {
-          localFile {
-            publicURL
-            childImageSharp {
-              id
-              fixed {
-                srcSet
-                src
-              }
+            fixed {
+              srcSet
+              src
             }
           }
         }
+      }
     }
   }
-`;
+`
